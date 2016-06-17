362dab834ec326718c6c297f599e5ffbb6db7238
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-06-16 23:30:58 -0700

    Debugging

---
+++ src/main/scala/processors/package.scala
@@ -0,0 +1,20 @@
+import com.typesafe.scalalogging.LazyLogging
+import edu.arizona.sista.processors
+
+
+package object processors extends LazyLogging {
+
+  def displaySentence(sentence: processors.Sentence): Unit = {
+    logger.debug(s"""
+        |processors.Sentence words: ${sentence.words.mkString(" ")}
+        |processors.Sentence startOffsets: ${sentence.startOffsets.mkString(" ")}
+        |processors.Sentence endOffsets: ${sentence.endOffsets.mkString(" ")}
+        |processors.Sentence lemmas: ${sentence.lemmas.get.mkString(" ")}
+        |processors.Sentence tags: ${sentence.tags.get.mkString(" ")}
+        |processors.Sentence entities: ${sentence.entities.get.mkString(" ")}
+        |dependency edges: ${sentence.dependencies.get.allEdges.mkString(" ")}
+        |dependency roots: ${sentence.dependencies.get.roots.mkString(" ")}
+    """.stripMargin)
+  }
+
+}
