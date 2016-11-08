f4cf40792b7b0d3bfdca7341f2733c81327511fe
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-11-07 19:55:03 -0700

    prefix with "api"

--- src/main/scala/service/Service.scala
+++ src/main/scala/service/Service.scala
@@ -1,19 +1,19 @@
 package service
 
 import java.io.File
-
 import akka.actor.ActorSystem
-import akka.event.{LoggingAdapter, Logging}
-import akka.http.javadsl.model.MediaTypes
-import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport._
-import akka.http.scaladsl.model.{HttpResponse, HttpEntity, ContentTypes}
+import akka.event.LoggingAdapter
 import de.heikoseeberger.akkahttpjson4s.Json4sSupport
 import scala.concurrent.duration._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.Materializer
-import processors.{api, ProcessorsBridge, ConverterUtils}
+import processors.{ConverterUtils, ProcessorsBridge, api}
 import org.clulab.processors
-import org.json4s.{Formats, DefaultFormats, jackson, native}
+import org.json4s.JsonDSL._
+import org.json4s._
+import org.json4s.native.JsonMethods._
+import org.json4s.JsonAST.{JNothing, JString}
+import org.json4s.{DefaultFormats, Formats, JValue, jackson, native}
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.ExecutionContextExecutor
 import com.typesafe.config.Config
@@ -73,76 +73,81 @@ trait Service extends Json4sSupport {
             complete(utils.mkDescription)
         } ~
         // Handle parsing, etc
-        path("annotate") {
+        path("api" / "annotate") {
           entity(as[api.TextMessage]) { m =>
             logger.info(s"Default Processor received POST with text -> ${m.text}")
             val processorsDoc = ProcessorsBridge.annotate(m.text)
-            val doc = ConverterUtils.toDocument(processorsDoc)
-            complete(doc)
+            val json = ConverterUtils.toJSON(processorsDoc)
+            complete(json)
           }
         } ~
           // annotate from sentences
-          path("annotate") {
+          path("api" / "annotate") {
             entity(as[api.SentencesMessage]) { sm =>
               logger.info(s"Default Processor received POST with text already split into sentences...")
               val processorsDoc = ProcessorsBridge.annotate(sm.sentences)
-              val doc = ConverterUtils.toDocument(processorsDoc)
-              complete(doc)
+              val json = ConverterUtils.toJSON(processorsDoc)
+              complete(json)
             }
           } ~
-          path("fastnlp" / "annotate") {
+          path("api" / "fastnlp" / "annotate") {
             entity(as[api.TextMessage]) { m =>
               logger.info(s"FastNLPProcessor received POST with text -> ${m.text}")
               val processorsDoc = ProcessorsBridge.annotateWithFastNLP(m.text)
-              val doc = ConverterUtils.toDocument(processorsDoc)
-              complete(doc)
+              val json = ConverterUtils.toJSON(processorsDoc)
+              complete(json)
             }
           } ~
           // annotate from sentences
-          path("fastnlp" / "annotate") {
+          path("api" / "fastnlp" / "annotate") {
             entity(as[api.SentencesMessage]) { sm =>
               logger.info(s"FastNLPProcessor received POST with text already segmented into sentences ")
               val processorsDoc = ProcessorsBridge.annotateWithFastNLP(sm.sentences)
-              val doc = ConverterUtils.toDocument(processorsDoc)
-              complete(doc)
+              val json = ConverterUtils.toJSON(processorsDoc)
+              complete(json)
             }
           } ~
-          path("bionlp" / "annotate") {
+          path("api" / "bionlp" / "annotate") {
             entity(as[api.TextMessage]) { m =>
               logger.info(s"BioNLPProcessor received POST with text -> ${m.text}")
               val processorsDoc = ProcessorsBridge.annotateWithBioNLP(m.text)
-              val doc = ConverterUtils.toDocument(processorsDoc)
-              complete(doc)
+              val json = ConverterUtils.toJSON(processorsDoc)
+              complete(json)
             }
           } ~
           // annotate from sentences
-          path("bionlp" / "annotate") {
+          path("api" / "bionlp" / "annotate") {
             entity(as[api.SentencesMessage]) { sm =>
               logger.info(s"BioNLPProcessor received POST with text already segmented into sentences ")
               val processorsDoc = ProcessorsBridge.annotateWithBioNLP(sm.sentences)
-              val doc = ConverterUtils.toDocument(processorsDoc)
-              complete(doc)
+              val json = ConverterUtils.toJSON(processorsDoc)
+              complete(json)
             }
           } ~
           // Handle sentiment analysis of text
-          path("sentiment" / "corenlp" / "score") {
+          path("api" / "sentiment" / "corenlp" / "score") {
             entity(as[api.TextMessage]) { m =>
               logger.info(s"CoreNLPSentimentAnalyzer received POST with text -> ${m.text}")
               val scores = ProcessorsBridge.toSentimentScores(m.text)
               complete(scores)
             }
           } ~
-          // Handle sentiment analysis of a Sentence
-          path("sentiment" / "corenlp" / "score") {
-            entity(as[api.Sentence]) { s =>
-              logger.info(s"CoreNLPSentimentAnalyzer received Sentence in POST with text -> ${s.words.mkString(" ")}")
-              val sentence: processors.Sentence = ConverterUtils.toProcessorsSentence(s)
-              val scores = ProcessorsBridge.toSentimentScores(sentence)
-              complete(scores)
+          path("api" / "sentiment" / "corenlp" / "score") {
+            entity(as[JValue]) {
+              // Handle sentiment analysis of a Sentence
+              case s: JValue if s \ "words" != JNothing =>
+                val sentence = ConverterUtils.toProcessorsSentence(s)
+                val scores = ProcessorsBridge.toSentimentScores(sentence)
+                complete(scores)
+              // Handle sentiment analysis of a Document
+              case d: JValue if d \ "sentences" != JNothing =>
+                val document = ConverterUtils.toProcessorsDocument(d)
+                val scores = ProcessorsBridge.toSentimentScores(document)
+                complete(scores)
             }
           } ~
           // Handle sentiment analysis of a seq of text
-          path("sentiment" / "corenlp" / "score" / "segmented") {
+          path("api" / "sentiment" / "corenlp" / "score" / "segmented") {
             entity(as[api.SentencesMessage]) { sm =>
               logger.info(s"CoreNLPSentimentAnalyzer received POST with text already segmented into sentences")
               val sentences: Seq[String] = sm.sentences
@@ -150,46 +155,48 @@ trait Service extends Json4sSupport {
               complete(scores)
             }
           } ~
-          // Handle sentiment analysis of a Document
-          path("sentiment" / "corenlp" / "score") {
-            entity(as[api.Document]) { doc =>
-              logger.info(s"CoreNLPSentimentAnalyzer received Document in POST with text -> ${doc.text}")
-              val document: processors.Document = ConverterUtils.toProcessorsDocument(doc)
+          path("api" / "sentiment" / "corenlp" / "score") {
+            entity(as[JValue]) { json =>
+              val document: processors.Document = ConverterUtils.toProcessorsDocument(json)
               val scores = ProcessorsBridge.toSentimentScores(document)
               complete(scores)
             }
           } ~
           // Handle IE with Odin
-          path("odin" / "extract") {
+          path("api" / "odin" / "extract") {
             entity(as[api.DocumentWithRules]) { dwr =>
               logger.info(s"Odin endpoint received DocumentWithRules")
               val document = ConverterUtils.toProcessorsDocument(dwr.document)
               val mentions = ProcessorsBridge.getMentions(document, dwr.rules)
-              complete(mentions)
+              val json = ConverterUtils.toJSON(mentions)
+              complete(json)
             }
           } ~
-          path("odin" / "extract") {
+          path("api" / "odin" / "extract") {
             entity(as[api.DocumentWithRulesURL]) { dwu =>
               logger.info(s"Odin endpoint received DocumentWithRulesURL")
               val document = ConverterUtils.toProcessorsDocument(dwu.document)
               val mentions = ProcessorsBridge.getMentions(document, ConverterUtils.urlToRules(dwu.url))
-              complete(mentions)
+              val json = ConverterUtils.toJSON(mentions)
+              complete(json)
             }
           } ~
-          path("odin" / "extract") {
+          path("api" / "odin" / "extract") {
             entity(as[api.TextWithRules]) { twr =>
               logger.info(s"Odin endpoint received TextWithRules")
               val document = ProcessorsBridge.annotateWithFastNLP(twr.text)
               val mentions = ProcessorsBridge.getMentions(document, twr.rules)
-              complete(mentions)
+              val json = ConverterUtils.toJSON(mentions)
+              complete(json)
             }
           } ~
-          path("odin" / "extract") {
+          path("api" / "odin" / "extract") {
             entity(as[api.TextWithRulesURL]) { twu =>
               logger.info(s"Odin endpoint received TextWithRulesURL")
               val document = ProcessorsBridge.annotateWithFastNLP(twu.text)
               val mentions = ProcessorsBridge.getMentions(document, ConverterUtils.urlToRules(twu.url))
-              complete(mentions)
+              val json = ConverterUtils.toJSON(mentions)
+              complete(json)
             }
           } ~
           // shuts down the server
